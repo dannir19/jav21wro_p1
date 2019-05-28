f09b5f4e02a6ad8515e0413eca660a88d95f83e2
dannir19 <d.krauz@op.pl>
2019-05-28 22:50:31 +0200

    zadania

--- src/com/sda/HomeTask/RepeatElementMain.java
+++ src/com/sda/HomeTask/RepeatElementMain.java
@@ -5,11 +5,12 @@ public class RepeatElementMain {
 
     public static void main(String[] args) {
 
-    }
+
     RepeatElement repeat = new RepeatElement();
     int arr[] = {4, 2, 4, 5, 2, 3, 1};
     int arr_size = arr.length;
     repeat.printRepeating(arr, arr_size);
     }
 }
+
 
